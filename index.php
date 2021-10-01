bdd19f31a0b0e9a46e030a19b1125d0f822eb8a8
Timur Myngbay <myngbay.t@rahmetapp.kz>
2021-10-01 15:05:01 +0600

    Demonstrate adapter pattern

--- index.php
+++ index.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 require_once 'vendor/autoload.php';
 
 use App\Examples\Patterns\Adapter\Book;
+use App\Examples\Patterns\Adapter\BookAdapter;
 
 $book = new Book('Timur', 'Body');
+$bookAdapter = new BookAdapter($book);
 
-customPrint($book->getAuthor());
+customPrint($bookAdapter->getAuthorAndTitle());
